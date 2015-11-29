d26effe6d5cfaf0aa734cb608aa2de18bdafc3f9
Sebastian Wahsner <xcozmox@posteo.de>
2015-11-29 16:06:20 +0100

    initial commit

---
+++ client/components/auth/user.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('mstockApp')
+  .factory('User', function ($resource) {
+    return $resource('/api/users/:id/:controller', {
+      id: '@_id'
+    },
+    {
+      changePassword: {
+        method: 'PUT',
+        params: {
+          controller:'password'
+        }
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id:'me'
+        }
+      }
+    });
+  });
